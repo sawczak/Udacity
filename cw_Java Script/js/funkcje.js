99e439bf83baec57d69b2500413cb34db27d55e6
Izabela Sawczak <izabela.ziemba@gmail.com>
2019-06-03 22:33:25 +0200

    add functions

---
+++ cw_Java Script/js/funkcje.js
@@ -0,0 +1,42 @@
+// declares the sayHello function
+function sayHello() {
+    let message = "Hello!"
+    return message; // returns value instead of printing it
+  }
+
+
+
+
+
+// zapis standardowy
+/* const mnozenie = function(liczba1, liczba2) {
+    // console.log("w środku funkcji");
+    // console.log(liczba1, liczba2);
+    let wynik = liczba1 * liczba2;
+
+    return wynik;
+};
+ */
+// console.log(mnozenie(2, 3));
+
+
+// zapis strzałkowy
+
+/* const mnozenie = (liczba1, liczba2) => liczba1 * liczba2;
+console.log(mnozenie(2, 3));
+ */
+
+/* const mnozenie = (liczba1, liczba2) => {
+    return liczba1 * liczba2;
+};
+
+console.log(mnozenie(2, 2));
+ */
+
+/* const mnozenie = liczba => liczba * 2;
+
+
+console.log(mnozenie(4)); 
+
+
+ */
