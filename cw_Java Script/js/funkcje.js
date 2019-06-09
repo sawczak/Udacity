9f50e9fddf18297145ee6804f4bf84f2d28c3fb3
Izabela Sawczak <izabela.ziemba@gmail.com>
2019-06-09 09:53:15 +0200

    exercise jS

--- cw_Java Script/js/funkcje.js
+++ cw_Java Script/js/funkcje.js
@@ -34,7 +34,7 @@
 
 
 //function with loop
-  function laugh(num) {
+/*   function laugh(num) {
 
     let lenghtLaugh = '';
     for(let i = 0; i < num; i++) {
@@ -46,6 +46,81 @@
   }
   
   console.log(laugh(8));
+ */
+
+
+//function with integer
+/* function isPrime(integer) {
+    for(let x = 2; x < integer; x++) {
+        if(integer % x === 0) {
+            console.log(integer + " is divisible by " + x);
+            return false
+        }
+    }
+    return true;
+}
+
+isPrime(15); */
+
+
+
+
+//return and print are not the same thing
+/* function isThisWorking(input) {
+    console.log("Printing: isThisWorking was called and " + input + " was passed in as an argument.");
+    return "Returning: I am returning this string!";
+  }
+  
+  isThisWorking(3);
+ */
+
+
+
+  //exercise
+/*   function sleep() {
+    console.log("I'm sleepy!");
+    return "zzz";
+    return "snore";
+  }
+  
+  sleep(); */
+//The function will return "zzz" and print "I'm sleepy!" to the console. "snore" will not be returned since the first return statement will exit the function.
+
+
+
+
+
+// returns the sum of two numbers
+function add(x, y) {
+    return x + y;
+  }
+  
+  // returns the value of a number divided by 2
+  function divideByTwo(num) {
+    return num / 2;
+  }
+  
+  var sum = add(5, 7); // call the "add" function and store the returned value in the "sum" variable
+  var average = divideByTwo(sum); // call the "divideByTwo" function and store the returned value in the "average" variable
+
+
+
+//  exercise, results = 4
+function addTen(x) {
+    return x + 10;
+  }
+  
+  function divideByThree(y) {
+    return y / 3;
+  }
+  
+  var result = addTen(2);
+  console.log(divideByThree(result));
+
+
+
+
+
 
 
 
