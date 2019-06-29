0945dd6a52c2c0a55c3077375e405787717dd883
Izabela Sawczak <izabela.ziemba@gmail.com>
2019-06-29 09:08:53 +0200

    hoisting

--- cw_Java Script/js/scope_hoisting.js
+++ cw_Java Script/js/scope_hoisting.js
@@ -30,3 +30,38 @@ addTwo();
 x = x + 1;
 console.log(x); */
 // wynik = 2
+
+
+
+
+
+
+
+
+
+
+
+
+// HOISTING
+sayHi("Julia");
+
+function sayHi(name) {
+  console.log(greeting + " " + name);
+  var greeting;
+}
+
+/* The function declaration is hoisted to the top of its current scope, and inside the function, the greeting variable declaration is also hoisted to the top of its function scope. */
+
+/* function sayHi(name) {
+  var greeting = "Hello";
+  console.log(greeting + " " + name);
+}
+
+sayHi("Julia"); */
+
+
+
+/* What you've learned so far:
+JavaScript hoists function declarations and variable declarations to the top of the current scope.
+Variable assignments are not hoisted.
+Declare functions and variables at the top of your scripts, so the syntax and behavior are consistent with each other. */
